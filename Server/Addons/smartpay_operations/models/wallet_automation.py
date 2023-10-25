556a07ea2d53fc10fdb150b5893b9da1c1669bb4
Mohammed Ashraf <hom9244.9244@gmail.com>
2023-10-26 02:00:29 +0300

    [IMP]smartpay_operations:Improve wallet automation

--- Server/Addons/smartpay_operations/models/wallet_automation.py
+++ Server/Addons/smartpay_operations/models/wallet_automation.py
@@ -18,30 +18,54 @@ class AutomateSalesWallet(models.Model):
     tag_ids = fields.Many2many('res.partner.category', column1='partner_id',
                                column2='wallet_id', string='Tags')
     wallet_balance = fields.Float('Wallet Balance', default=1.0, required=True)
-    condition_operator = fields.Selection([('equal', 'Equal'), ('gt', 'Greater Than'), ('lt', 'Less Than')],
-                                          'Balance Operator',
-                                          default='equal', required=True)
-    wallet_status = fields.Selection([('active', 'Active'), ('inactive', 'Inactive')], 'Wallet Status',
+    condition_operator = fields.Selection([
+        ('equal', 'Equal'),
+        ('gt', 'Greater Than'),
+        ('lt', 'Less Than')
+    ],
+        string='Balance Operator',
+        default='equal', required=True)
+    wallet_status = fields.Selection([('active', 'Active'), ('inactive', 'Inactive')],
+                                     string='Wallet Status',
                                      default='active', required=True)
-    action_taken = fields.Selection([('inactive_w', 'Deactivate Wallet'), ('activate_w', 'Activate Wallet'),
-                                     ('inactivate_c', 'Deactivate Customer'), ('activate_u', 'Activate User')],
-                                    string='Action To Be Taken', default='active', required=True)
+    action_taken = fields.Selection([
+        ('inactive_w', 'Deactivate Wallet'),
+        ('activate_w', 'Activate Wallet'),
+        # ('inactivate_c', 'Deactivate Customer'),
+        ('activate_c', 'Activate Customer'),
+        ('activate_u', 'Activate User'),
+        ('inactivate_u', 'Deactivate User'),
+    ],
+        string='Action To Be Taken', default='active', required=True)
+
+    active_record = fields.Boolean("Active Record", default=True)
 
     def automate_wallet(self, action_name=None):
         if action_name:
+            _logger.info("--> Running Automate Wallet")
             wallet_automation = self.env['wallet.automation'].search(
                 [('name', 'ilike', action_name)], limit=1)
+            _logger.info(f"wallet_automation: {wallet_automation}")
             if wallet_automation:
                 if wallet_automation.tag_ids:
+                    active_record = wallet_automation.active_record is True
                     customers = self.env['res.partner'].search([
-                        ('category_id', 'in', wallet_automation.tag_ids.ids)])
+                        ('category_id', 'in', wallet_automation.tag_ids.ids),
+                        ('active', '=', active_record)
+                    ])
+
+                    _logger.info(f"customers {customers}")
                     wallet_status = wallet_automation.wallet_status == 'active'
-                    for rec in customers:
+                    _logger.info(f"wallet_status {wallet_status}")
+
+                    for rec in customers.sudo():
                         wallets = rec.wallet_ids.filtered(
-                            lambda x: x.type == wallet_automation.wallet_type_id.name and
+                            lambda x: x.type == wallet_automation.wallet_type_id and
                                       x.active == wallet_status
                         )
+                        _logger.info(f"wallets {wallets}")
                         wallet = wallets and wallets[0]
+                        _logger.info(f"wallet {wallet}")
                         if not wallet:
                             continue
                         # if wallet_automation.action_taken in ['inactive_w', 'activate_w']:
@@ -53,7 +77,8 @@ class AutomateSalesWallet(models.Model):
                                 and
                                 not (wallet.balance_amount < wallet_automation.wallet_balance)):
                             continue
-                        if wallet.balance_amount != wallet_automation.wallet_balance:
+                        if wallet_automation.condition_operator == 'equal' and \
+                                wallet.balance_amount != wallet_automation.wallet_balance:
                             continue
 
                         if wallet_automation.action_taken == 'inactive_w':
@@ -62,6 +87,12 @@ class AutomateSalesWallet(models.Model):
                             wallet.active = True
                         elif wallet_automation.action_taken == 'activate_u':
                             rec.user_ids.write({'active': True})
-                        elif wallet_automation.action_taken == 'inactivate_c':
-                            rec.write({'active': False})
+                        elif wallet_automation.action_taken == 'inactivate_u':
+                            rec.user_ids.write({'active': False})
+                        # elif wallet_automation.action_taken == 'inactivate_c':
+                        #     rec.user_ids.write({'active': False})
+                        #     self._cr.commit()
+                        #     rec.write({'active': False})
+                        elif wallet_automation.action_taken == 'activate_c':
+                            rec.user_ids.write({'active': True})
                 return
